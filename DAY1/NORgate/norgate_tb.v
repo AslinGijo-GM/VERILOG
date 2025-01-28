0db126cd8bd3f994c9fed20cc8a33f6719a991b1
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-01-28 12:24:54 +0530

    Create norgate_tb.v

---
+++ DAY1/NORgate/norgate_tb.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+
+module norgate_tb;
+    
+    reg a,b ;
+    wire c;
+    
+    nandgate uut(.a(a),.b(b),.c(c));
+    initial begin 
+    a=0;b=0;#10;
+    a=0;b=1;#10;
+    a=1;b=0;#10;
+    a=1;b=1;#10;
+    $finish;
+    end
+endmodule
