db3c767efe7d4f04e3bcf616b07a3f9d88838286
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-01-29 15:38:19 +0530

    Update cla.v

--- DAY2/CLA/cla.v
+++ DAY2/CLA/cla.v
@@ -13,9 +13,9 @@ assign p = a^b;
 
 assign c[0] = cin;
 assign c[1]= g[0]  | (p[0]& cin);
-assign c[2]= g [1] | (p[1] & g[0] ) | (p[0] & p[1] & cin)  ;
-assign c[3]= g[2]  | (p[2]& g[1])   | (p[2] & p[1] & g[0] )       | (p[0] & p[1] &p[2] & cin)  ;
-assign c[4] = g[3] | (p[3] & g[2])  | (p[2] & p[1] & p[0] &g[1])  |(p[3] & p[2] & p[1] & g[0]) | (p[0] & p[1] & p[2] & p [3] &cin); 
+assign c[2]= g [1] | (p[1] &c[1] )  ;
+assign c[3]= g[2]  | (p[2]& c[2])  ;
+assign c[4] = g[3] | (p[3] & c[3]); 
 
 assign sum = p ^ c[3:0] ;
 
