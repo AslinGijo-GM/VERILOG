3fe0821b7eea0a0bc31b23a0c4e756b1e556199f
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-02-03 10:41:09 +0530

    Update piso_tb.v

--- DAY5/PISO/piso_tb.v
+++ DAY5/PISO/piso_tb.v
@@ -1,6 +1,7 @@
 module piso_tb;
 reg clk;
 reg reset;
+reg load;
 reg [4:0]pi;
 wire so;
 
@@ -8,20 +9,22 @@ piso uut(
 .clk(clk),
 .reset(reset),
 .pi(pi),
-.so(so)
+.so(so),
+.load(load)
 );
 initial begin
     clk =0;
     forever #5 clk = ~clk;
 end
 initial begin
-    reset = 1; pi = 5'b11010; #10;
-     reset = 0;#10;
-     reset = 0;#10;  
-     reset = 0;#10;  
-     reset = 0;#10;       
-     reset = 0;#10;
-     reset = 0;#10;
+     reset = 1;         pi = 5'b11010; #10;
+     reset = 0;load =1; pi = 5'b11010; #10;
+     reset = 0;load =0;                #10;
+     reset = 0;load =0;                #10;  
+     reset = 0;load =0;                #10;  
+     reset = 0;load =0;                #10;       
+     reset = 0;load =0;                #10;
+     reset = 0;load =0;                #10;
 
     $finish;
 end
