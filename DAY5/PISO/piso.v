baaa2afd80c9965f6f49bac26598ee0b1d4cea42
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-02-03 10:38:40 +0530

    Update piso.v

--- DAY5/PISO/piso.v
+++ DAY5/PISO/piso.v
@@ -1,14 +1,17 @@
 module piso(
 input clk,
 input reset,
+input load,
 input [4:0]pi,
 output reg so
 );
 reg [4:0]sft;
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        sft <= pi;
-        
+        sft <= 5'b00000;
+    end
+    else if (load) begin
+        sft <= pi;    
     end
     else begin
         sft <= {1'b0,sft[4:1]};
