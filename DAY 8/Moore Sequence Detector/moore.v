6e86e031adca12497e5d111a9b8e78f97b17ca3e
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-02-09 19:41:30 +0530

    Create moore.v

---
+++ DAY 8/Moore Sequence Detector/moore.v
@@ -0,0 +1,42 @@
+module moore(
+    input clk,
+    input reset,
+    input in,
+    output reg out
+);
+    parameter [2:0] 
+    s0=3'b000,
+    s1=3'b001,
+    s2=3'b010,
+    s3=3'b011,
+    s4=3'b100
+    ;
+    
+    reg [2:0]next,state;
+    
+    always @(posedge clk or posedge reset) begin
+        if(reset)
+            state <= s0 ;
+        else
+            state <=next;
+    end
+    
+    always @(*) begin
+        next =state;
+        case(state)
+            s0: next = in ? s1 : s0 ;
+            s1: next = in ? s1 : s2 ;
+            s2: next = in ? s3 : s0 ;
+            s3: next = in ? s4 : s0 ;
+            s4: next = in ? s1 : s0 ;
+            default : next = s0;
+        endcase
+    end
+    
+    always @(*) begin
+        
+        out =(state == s4) ;
+        
+    end
+    
+endmodule
