0de8da8dfaea6b1720deb8834d35427abb8816d4
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-02-09 19:40:53 +0530

    Create  mealy_tb.v

---
+++ DAY 8/Mealy Sequence Detector/mealy_tb.v
@@ -0,0 +1,35 @@
+module mealy_tb;
+reg clk;
+reg reset;
+reg in;
+wire out;
+
+mealy uut (
+.clk(clk),
+.reset(reset),
+.in(in),
+.out(out)
+);
+initial begin
+    clk=0;
+    forever #5 clk=~clk;
+end
+
+
+initial begin
+         reset = 1; in = 0; 
+        #10 reset = 0;              
+        
+        
+        #10 in = 1;  
+        #10 in = 0;  
+        #10 in = 1;  
+        #10 in = 0;  
+        #10 in = 1;  
+        #10 in = 1;  
+        #10 in = 0;  
+        #10 in = 0;  
+        #20 $finish; 
+
+end
+endmodule
