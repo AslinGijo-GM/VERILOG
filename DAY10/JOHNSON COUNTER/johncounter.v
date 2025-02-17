f78e07ed49d2c29240cdec6a774fb2c2e2763c22
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-02-17 18:03:00 +0530

    Create johncounter.v

---
+++ DAY10/JOHNSON COUNTER/johncounter.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module johncounter(
+    input [3:0]din,
+    input clk,
+    input reset,
+    output reg [3:0]dout
+    );
+    
+        reg [3:0]mem;
+        
+    always @(posedge clk or posedge reset) begin
+        if(reset) begin
+            mem <=din;
+        end
+        else begin
+            mem <= { mem[2:0],~mem[3] };
+        end
+        dout = mem;
+    end
+    
+endmodule
