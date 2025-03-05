4a6198911cbec0ae6bd71a0cf19c6652ece65fbf
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-03-05 21:35:50 +0530

    Create fsm_tb.v

---
+++ ASSIGNMENT/Assignment FSM/Detects the alternate 0’s and 1’s/fsm_tb.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+
+module fsm_tb;
+
+    parameter   s0 = 0,
+                s1 = 1,
+                s2 = 2,
+                s3 = 3,
+                s4 = 4,
+                s5 = 5,
+                s6 = 6;
+  
+        reg clk;
+        reg reset;
+        reg in;
+        wire out;
+  
+  fsm uut (
+    .clk(clk),
+    .reset(reset),
+    .in(in),
+    .out(out)
+  );
+  
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+  
+  initial begin
+    reset = 1;#10;reset = 0 ;
+    
+    in = 1;#10;
+    in = 1;#10;
+    in = 1;#10;
+    in = 0;#10;
+    in = 1;#10;
+    in = 0;#10;
+    in = 1;#10;
+    in = 1;#10;
+    in = 0;#10;
+    
+    $finish;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+                
+endmodule
