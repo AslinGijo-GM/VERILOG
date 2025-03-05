1a54a82d2732f5a4267800a2e6f6013f1835aa56
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-03-05 20:29:31 +0530

    Create fsm_tb.v

---
+++ ASSIGNMENT/Assignment FSM/Two states, two inputs/fsm_tb.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module fsm_tb;
+
+    parameter ON = 1,
+              OFF = 0;
+              
+    reg clk;
+    reg reset;
+    reg in;
+    wire out;
+    
+   fsm uut(
+   .clk(clk),
+   .reset(reset),
+   .in(in),
+   .out(out)
+   ); 
+   
+   initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+   end
+   
+   initial begin
+        reset = 1;#10;reset = 0;
+        in = 1;#10;
+        in = 0;#10;
+        in = 1;#10;
+        in = 1;#10;
+        $finish;
+        
+        
+   end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+  
+   
+    
+endmodule
